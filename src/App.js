405b25e6512acbf39a71fb8df3b63bc7164bb999
CJL Fortunato <clemence.fortunato@gmail.com>
2022-06-22 10:55:36 +0200

    Add basic HTML and CSS for pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Dashboard } from './Pages/Dashboard';
 import { Login } from './Pages/Login';
 import { Register } from './Pages/Register';
+import { Header } from './Components/Header';
 
 function App() {
   return (
     <>
       <Router>
         <div className="App">
-
+          <Header />
           <Routes>
             <Route path='/' element={<Dashboard />}></Route>
             <Route path='/login' element={<Login />}></Route>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export function Header() {
     return (
-        <div>Header</div>
+        <header>
+            <div className='logo'>
+                <Link to='/'>Achievements</Link>
+            </div>
+            <ul>
+                <li>
+                    <Link to='/login'>
+                        <i class="fa-solid fa-right-to-bracket"></i>
+                        Login
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/register'>
+                        <i class="fa-solid fa-user"></i>
+                        Register
+                    </Link>
+                </li>
+            </ul>
+        </header>
     )
 }
